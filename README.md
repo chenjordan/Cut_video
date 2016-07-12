f3e277644d6262ef848b17fe06a51543ee6949fc
john <john@viscovery.co>
2016-07-12 15:37:00 +0800

    add comment in run_cut_video_example

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## Synopsis
 parse videos under input video path and then cut video by cut period parameter
 
-## file note
+## File note
 cut_video.py: class to cut video per CUT_PERIOD seconds
-run_cut_video_example.py: an example of how to use Cutvideo class
+
+run_cut_video_example.py: an example of how to use Cut_video class
--- run_cut_video_example.py
+++ run_cut_video_example.py
@@ -7,16 +7,24 @@ if __name__ == "__main__":
 
     # define the number of processor
     NUM_PROCESS = 4
+
+    # define the cut video period
     CUT_PERIOD = 10
 
-    # output video root folder
+    # define output video root folder
     out_root_folder = './test_cut_output/'
+
+    # define in_video_path, the program will convert all mp4 file under this folder
     in_video_path = './tmp_video/*.mp4'
 
     cut_class = Cut_video(in_video_path, out_root_folder, NUM_PROCESS, CUT_PERIOD)
+
     cut_class.pre_processing()
+
     start_time = time.time()
+
     cut_class.start_process()
+
     end_time = time.time()
     cost = end_time - start_time
-    print 'cost time %d s' % cost
+    print 'Total cost time=> %d s' % cost
