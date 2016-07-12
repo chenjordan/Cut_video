75ba94946fb6ced59a6471b740990c3cd26be35e
john <john@viscovery.co>
2016-07-12 15:31:21 +0800

    remove unusage code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 #Cut_video
+
+## Synopsis
+parse videos under input video path and then cut video by cut period parameter
+
+## file note
+cut_video.py: class to cut video per CUT_PERIOD seconds
+run_cut_video_example.py: an example of how to use Cutvideo class
--- cut_video.py
+++ cut_video.py
@@ -80,20 +80,3 @@ class Cut_video:
         map(lambda pc: pc.join(), process)
 
 
-# if __name__ == "__main__":
-#
-#     # define the number of processor
-#     NUM_PROCESS = 4
-#     CUT_PERIOD = 10
-#     # output video root folder
-#     out_root_folder = './test_cut_output/'
-#     in_video_path = './tmp_video/*.mp4'
-#
-#     cut_class = Cut_video(in_video_path, out_root_folder, NUM_PROCESS, CUT_PERIOD)
-#     cut_class.pre_processing()
-#     start_time = time.time()
-#     cut_class.start_process()
-#     end_time = time.time()
-#     cost = end_time - start_time
-#     print 'cost time %d s' % cost
-
